d5eaf28e22f949a393dede77655ba8647ac88144
vifclem <clement.expert@artfx.fr>
2022-12-08 17:05:44 +0100

    [fixe] door

--- Assets/clem/door.cs
+++ Assets/clem/door.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class door : MonoBehaviour
 {
     public Animator animator;
-    private GameObject player;
+   
 
 
-    private void Awake()
-    {
-        player = GameObject.FindWithTag("Player");
-    }
+   
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Electric"))
         {
-            //jouer l'anim de la porte qui se leve 
+            
             
             animator.SetTrigger("Door");
 
