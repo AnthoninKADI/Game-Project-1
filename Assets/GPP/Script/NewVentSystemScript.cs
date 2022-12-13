82ced54875286a418a6101ecb8ee3439a8a61da2
Anthonin ARTFX <akadi@artfx.fr>
2022-12-13 15:00:02 +0100

    [WIP] Camera

--- Assets/GPP/Script/NewVentSystemScript.cs
+++ Assets/GPP/Script/NewVentSystemScript.cs
@@ -11,7 +11,7 @@ public class NewVentSystemScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
        
-        if (collision.CompareTag("Electric")) 
+        if (collision.CompareTag("Player") && Input.GetKeyDown(KeyCode.E)) 
         {
             if (!IsOpen)
             {
