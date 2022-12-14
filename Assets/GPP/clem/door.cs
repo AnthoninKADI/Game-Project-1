2e3651110db51863f86b42720a02ff69c803fedc
vifclem <clement.expert@artfx.fr>
2022-12-14 12:37:39 +0100

    [add] door

--- Assets/GPP/clem/door.cs
+++ Assets/GPP/clem/door.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class door : MonoBehaviour
 {
-    public Animator animator;
-   
+    public GameObject DoorOpen, DoorClose;
+    public bool IsOpen;
 
-
-   
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {                                                                                                                                                                               
-        if (collision.gameObject.CompareTag("Electric"))
+    {
+
+        if (collision.CompareTag("Interact"))
         {
-            
-            
-            animator.SetTrigger("Door");
-
+            if (!IsOpen)
+            {
+                IsOpen = true;
+                DoorOpen.SetActive(true);
+                DoorClose.SetActive(false);
+            }
+            else if (IsOpen)
+            {
+                IsOpen = false;
+                DoorOpen.SetActive(false);
+                DoorClose.SetActive(true);
+            }
         }
-
-
     }
 
-  }
+}
