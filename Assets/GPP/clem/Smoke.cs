197d042b4930449d5e914a06cf22d19c517998f7
L3o <74620207+leorchr@users.noreply.github.com>
2022-12-14 10:22:02 +0100

    [fix] camera animator

--- Assets/GPP/clem/Smoke.cs
+++ Assets/GPP/clem/Smoke.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 public class Smoke : MonoBehaviour
 {
     private GameObject player;
-    SpriteRenderer mysprite;
-
+   
 
     private void Awake()
     {
         player = GameObject.FindWithTag("Player");
-        mysprite = player.GetComponent<SpriteRenderer>();
+      
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -20,9 +19,10 @@ public class Smoke : MonoBehaviour
         if (collision.tag == "Player")
         {
             //tuer le player, lancer l'anim de whip noir et retour au checkpoint
-            //GameManager.instance.Die();
 
-            mysprite.color = new Color(255, 163, 0, 0);
+            GameManager.instance.Die();
+
+            
 
         }
         
