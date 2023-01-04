7de23926941aa0b1f6b38a7b032df24b5b7163b2
Gabbbbbbbbbb <gcochard@artfx.fr>
2023-01-04 11:10:19 +0100

    [LD]
    04/01/2023
    Player controller code modified.
    Moving platform pattern fixed.

--- Assets/GPP/Script/PlayerController/PlayerController.cs
+++ Assets/GPP/Script/PlayerController/PlayerController.cs
@@ -254,7 +254,7 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("platform"))
         {
             isGrounded = true;
         }
