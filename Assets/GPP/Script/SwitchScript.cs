3cd8e5f4c1853ce0eb5e5905e882f38b25510848
L3o <leogravillon@hotmail.fr>
2023-01-05 15:36:11 +0100

    [feat] lights off when picking document

--- Assets/GPP/Script/SwitchScript.cs
+++ Assets/GPP/Script/SwitchScript.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class SwitchScript : MonoBehaviour
 {
     public Sprite On;
     private door door;
+    public GameObject lights;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.CompareTag("Electric"))
+        if (collision.CompareTag("Interact"))
         {
             GetComponent<SpriteRenderer>().sprite = On;
             door.CountSwitch();
+            lights.GetComponent<Light2D>().intensity = 0;
         }
     }
 
