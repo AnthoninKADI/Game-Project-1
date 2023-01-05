efddbd7bec42dcefada29ae1ccbe03bf6f949445
L3o <leogravillon@hotmail.fr>
2023-01-05 18:57:54 +0100

    Document Script for transition

--- Assets/GPP/Script/DocumentScript.cs
+++ Assets/GPP/Script/DocumentScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 using UnityEngine.Rendering.Universal;
 
 public class DocumentScript : MonoBehaviour
@@ -8,6 +10,10 @@ public class DocumentScript : MonoBehaviour
     public Sprite On;
     private DoorDocument doorDocument;
     public GameObject lights;
+    public bool transition;
+    public GameObject mission2Briefing;
+    public GameObject nextMissionButton;
+    public string levelToLoad;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,11 +23,21 @@ public class DocumentScript : MonoBehaviour
             GetComponent<SpriteRenderer>().sprite = On;
             lights.GetComponent<Light2D>().intensity = 0;
             doorDocument.CountSwitch();
+            if (transition)
+            {
+                mission2Briefing.SetActive(true);
+                EventSystem.current.SetSelectedGameObject(nextMissionButton);
+            }
         }
     }
 
     public void SetDoorDocument(DoorDocument y)
     {
         doorDocument = y;
+    }
+
+    public void GoLevel2()
+    {
+        SceneManager.LoadScene(levelToLoad);
     }
 }
