95a5639f58bcee6d14082d6020e91ef8feb07554
AnthoninKADI <akadi@artfx.fr>
2023-01-18 13:36:17 +0100

    [Correction] Debut Correction Enemy

---
+++ Assets/EnemyImmoScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyImmoScript : MonoBehaviour
+{
+    [Header("Others")]
+    [SerializeField]
+    private Animator ShootAnimation;
+    [SerializeField]
+    private GameObject ExclamationMark, Gun;
+    private float respawnTime;
+
+
+    public void Start()
+    {
+        respawnTime = GameManager.instance.respawnTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Gun.SetActive(false);
+            ExclamationMark.SetActive(true);
+            Invoke("ExclamationTime", respawnTime);
+            Invoke("ShootingAnimation", 0.2f);
+            Invoke("ShootingAnimationOff", respawnTime + 1);
+            //Play Shoot Animation after 0.2s
+            //Respawn Player on previous checkpoint
+        }
+    }
+    private void ShootingAnimation()
+    {
+        ShootAnimation.SetBool("IsShooting", true);
+    }
+    private void ShootingAnimationOff()
+    {
+        ShootAnimation.SetBool("IsShooting", false);
+    }
+
+    private void ExclamationTime()
+    {
+        ExclamationMark.SetActive(false);
+    }
+}
+
+
--- Assets/GPP/Script/Enemies/EnemyDeath.cs
+++ Assets/GPP/Script/Enemies/EnemyDeath.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class EnemyDeath : MonoBehaviour
 {
     public Animator animator;
+    public GameObject ZoneDetection;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Electric"))
         {
             GetComponentInParent<EnemyPatrol>().Freeze();
+            ZoneDetection.SetActive(false);
             animator.SetBool("IsDead", true);
             Invoke("EnemyDie", 2f);
             
